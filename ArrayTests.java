c8732c133f6cf772c593f7aac339eca902807fdc
B2Bomber2 <156377868+B2Bomber2@users.noreply.github.com>
2024-02-01 14:57:47 -0800

    Fixed Reversed

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -14,5 +17,24 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test 
+  public void testReversed2(){
+    int[] array1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(array1));
+  }
+
+  @Test 
+  public void testReversed3(){
+    int[] array1 = {100,99,98};
+    assertArrayEquals(new int[]{98,99,100}, ArrayExamples.reversed(array1));
+  }
+
+  @Test 
+  public void testReverseInPlace2(){
+    int[] array1 = {100,99,98};
+    int[] result = {98,99,100};
+    assertArrayEquals(result, ArrayExamples.reversed(array1));
   }
 }
